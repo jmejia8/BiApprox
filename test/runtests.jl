4eadd1628cde9c33533a454673b86a26792d3784
Jesus Mejia <jesusmejded@gmail.com>
2019-02-08 00:26:27 -0600

    test updated

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,22 +6,37 @@ function test1()
     f(x,y; n=length(y)-1) = abs( 4x[n+1] - x[n+2] ) * y[end]^2 + sum(L.( abs.(x[1:n]-y[1:n]), x[n+1], x[n+2]))
     F(x,y; n=length(y)-1) = abs( 4x[n+1] - x[n+2] ) / (1+y[end]^2) + sum(L.( abs.(x[1:n]-y[1:n]), x[n+1], x[n+2]))
 
-    n = 10
-    x = 10rand(n+2)
-    y = zeros(n+1)
+    Ψ(X::Matrix) = begin
+        n = size(X,2)-2
+        Y = zeros(N, n+1)
+        Y[:,1:n] = X[:,1:n]
+        Y        
+    end
 
-    y[1:n] = x[1:n]
+    n = 5
+    N = 300
+    D_ul = n+2
+    D_ll = n+1
 
-    # println(F(x,y))
-    q1 = f(x,y) 
+    X = -10 .+ 20rand(N, D_ul)
+    X[:, n+1:n+2] = 2 .+ 8rand(N, 2)
 
-    x[n+2] = 4x[n+1]
+    Y = Ψ(X)
 
-    q2 = F(x,y) 
+    Fs =  [ F(X[i,:], Y[i,:]) for i =1:N ]
 
-    println(q1, " ", q2 )
 
-    q2 + q1 ≈ 0    
+    X_test = -10 .+ 20rand(N, D_ul)
+    X_test[:, n+1:n+2] = 2 .+ 8rand(N, 2)
+    Y_test = Ψ(X_test)
+    Fs_test =  [ F(X_test[i,:], Y_test[i,:]) for i =1:N ]
+    
+
+    F̂s_test, α = kernel_approx_ul(Fs,X_test, X,Y)
+
+    println(sum(Fs_test- F̂s_test)/N)
+
+    true
 
 
 end
