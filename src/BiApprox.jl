0bfb7de2d7cfc582e3bc00a64d7cfbf604a2d7ba
Jesús Mejía <jesusmejded@gmail.com>
2019-02-11 14:21:21 -0600

    fix datatypes

--- src/BiApprox.jl
+++ src/BiApprox.jl
@@ -2,11 +2,11 @@ module BiApprox
 
 using LinearAlgebra
 
-export kernel_approx_ul, approx_values, train_model
+export kernel_approx_ul, approx_values, train_model, F̂
 
-gaussKern(x, y; σ=1) = exp(- norm(x-y)^2 / (2σ^2) )
+gaussKern(x::Vector, y::Vector; σ=1) = exp(- norm(x-y)^2 / (2σ^2) )
 
-function train_model(Fs::Vector, X::Matrix, Y::Matrix, kernel)
+function train_model(Fs::Vector, X::Matrix, Y::Matrix; kernel::Function = gaussKern )
     """
     X is a N×D, that contains N row vectors in R^D
     Y like X
@@ -28,8 +28,8 @@ function train_model(Fs::Vector, X::Matrix, Y::Matrix, kernel)
 end
 
 
-F̂(x, α, X, kernel) = dot( α, [kernel(x, X[i,:]) for i = 1:size(X,1)]  )
-approx_values(α::Vector, X::Matrix, X_data::Matrix, kernel) = [F̂(X[i,:], α, X_data, kernel) for i = 1:size(X, 1)]
+F̂(x::Vector, α::Vector, X::Matrix; kernel::Function = gaussKern) = dot( α, [kernel(x, X[i,:]) for i = 1:size(X,1)]  )
+approx_values(α::Vector, X::Matrix, X_data::Matrix; kernel::Function = gaussKern) = [F̂(X[i,:], α, X_data, kernel) for i = 1:size(X, 1)]
 
 function kernel_approx_ul(Fs::Vector,
                      X_test::Matrix,
