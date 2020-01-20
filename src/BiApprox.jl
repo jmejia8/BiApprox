ff40b1a4debc84e4ffe202b9aec5c4ba30a004a1
Jesús Mejía <jesusmejded@gmail.com>
2020-01-19 22:36:10 -0600

    exporting new stuff

--- src/BiApprox.jl
+++ src/BiApprox.jl
@@ -1,13 +1,16 @@
 module BiApprox
 
 import LinearAlgebra: norm, dot
-import MLKernels: Kernel, kernel, GaussianKernel
+import MLKernels: Kernel, kernel, GaussianKernel, PeriodicKernel
 import Random: randperm
+import Statistics: mean, std, var
 
 
 export kernel_approx_ul, approx_values, train_model, F̂
 export train, train!, evaluate, KernelInterpolation, Data
 export approximate
+
+export LL_approx, bruteforce!, Ψ_approx, update_approximation_errors!, fit!
 
 include("structures.jl")
 include("kernelInterpolation.jl")
